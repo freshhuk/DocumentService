8b2d9b2b6c683d4e420913885d9f370261507f1b
freshhuk <freshhuk13@gmail.com>
2024-09-22 00:09:49 +0300

    Start working

--- src/main/java/com/document/documentservcice/documentservice/Controllers/APIController.java
+++ src/main/java/com/document/documentservcice/documentservice/Controllers/APIController.java
@@ -1,4 +1,16 @@
 package com.document.documentservcice.documentservice.Controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class for end-point and working with other microservice
+ */
+@RestController
 public class APIController {
+
+    public ResponseEntity<String> uploadDocument(){
+        return null;
+    }
+    //как вариант для регестрации скинуть новый микросревис под эту задачу
 }
